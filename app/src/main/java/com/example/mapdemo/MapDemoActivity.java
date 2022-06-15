7fd78e0afacf64ca92627fb8902eef6f93a130c2
christymarc <cemb2020@mymail.pomona.edu>
2022-06-15 10:54:06 -0700

    bounce animation

--- app/src/main/java/com/example/mapdemo/MapDemoActivity.java
+++ app/src/main/java/com/example/mapdemo/MapDemoActivity.java
@@ -7,11 +7,13 @@ import android.content.DialogInterface;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
+import android.os.SystemClock;
 import android.text.Layout;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -268,6 +270,8 @@ public class MapDemoActivity extends AppCompatActivity implements GoogleMap.OnMa
                         .title(title)
                         .snippet(snippet)
                         .icon(defaultMarker));
+
+                dropPinEffect(marker);
             }
         });
         alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel",
@@ -279,6 +283,38 @@ public class MapDemoActivity extends AppCompatActivity implements GoogleMap.OnMa
                 });
 
         alertDialog.show();
+    }
+
+    private void dropPinEffect(final Marker marker) {
+        // Handler allows us to repeat a code block after a specified delay
+        final android.os.Handler handler = new android.os.Handler();
+        final long start = SystemClock.uptimeMillis();
+        final long duration = 1500;
+
+        // Use the bounce interpolator
+        final android.view.animation.Interpolator interpolator =
+                new BounceInterpolator();
+
+        // Animate marker with a bounce updating its position every 15ms
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                // Calculate t for bounce based on elapsed time
+                float t = Math.max(
+                        1 - interpolator.getInterpolation((float) elapsed
+                                / duration), 0);
+                // Set the anchor
+                marker.setAnchor(0.5f, 1.0f + 14 * t);
+
+                if (t > 0.0) {
+                    // Post this event again 15ms from now.
+                    handler.postDelayed(this, 15);
+                } else { // done elapsing, show window
+                    marker.showInfoWindow();
+                }
+            }
+        });
     }
 
     // Define a DialogFragment that displays the error dialog
